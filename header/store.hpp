cce5a75314dc5cfc70df8180b6ce91f8b6378db9
jzhan591 <jennz.zng@gmail.com>
2023-11-22 01:10:27 -0800

    push store.hpp

--- header/store.hpp
+++ header/store.hpp
@@ -1,4 +1,33 @@
-#ifndef STORE_H
-#define STORE_H
+#ifndef STORE_HPP
+#define STORE_HPP
 
+class Store
+{
+    private:
+    
+        // description for item
+        string itemDescription ; 
+        
+        // name of item
+        string itemName ;
 
+    public:
+
+        // array holding items and their info in the store
+        string storeContents[] ; 
+
+        // display everything in store in a table like format
+        void updateStoreContents() ; 
+
+        // display items in store in a table? format; shows quantity, description, name, etc.
+        void displayStoreContents() ; 
+
+        // display quantity of user requested item; not sure if should count directly or display quantity 
+        int displayItemQuantity( string userItem ) ;
+
+        // display description for each user requested item 
+        void displayItemDescription( string userItem ) ; 
+ 
+}
+
+#endif 
---
+++ store.cpp
@@ -0,0 +1,2 @@
+#include "../include/store.h"
+
