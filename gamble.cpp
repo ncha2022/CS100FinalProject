fec193235611600cea19dac031a5bc9d88b29615
jwang705 <jwang705@ucr.edu>
2023-11-23 17:49:26 -0800

    gamble function adjustments. still incomplete

--- gamble.cpp
+++ gamble.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 void gamble() { //need adjustments for accounting for opponent's stats
     //assuming stats are in integer form or converted and are in a range of 1-100
@@ -21,17 +22,25 @@ void gamble() { //need adjustments for accounting for opponent's stats
         cin >> input;
         if (input != 'y' || input != 'n') {
             cout << "Invalid input. Please enter 'y' for yes or 'n' for no\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     }
 
     if (input == 'y') {
-        //figure out money gambling mechanic and how it will affect action
+        // gamble mechanic will generate a payout of # multiplier depending on numbers bet on
+        cout << "What numbers are you betting on from 1 to 20?\n";
+        //create array of pointers? need to figure out how to properly store and read input
     }
     else {
         //change chance formula according to action stats (whether it depends on strength, wisdom, intelligence, etc)
+        cout << "You have decided not to stake any money.\n";
         chance = (strength + dex + consitution + intelligence + wisdom + charisma) / 6;
         cout << "You have a " << chance << "% chance of effectiveness\n";
         damage = damage * chance;
         //either print out opponent health or display accordingly based on damage adjustment
     }
+
+    //dice roll function
+
 }
