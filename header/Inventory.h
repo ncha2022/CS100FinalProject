b4d72284ebce2589bb5d02267ed275096a086741
anishkancherla <anish.kancherla@gmail.com>
2023-11-29 02:13:26 -0800

    Updated inventory to add hasItem method

--- header/Inventory.h
+++ header/Inventory.h
@@ -6,18 +6,21 @@
 #include <string>
 
 class Item {
-    public:
-        std::string name;
-        Item(std::string name);
+public:
+    std::string name;
+    std::string description;
+    int quantity;
+    Item(std::string name, std::string desc, int qty);
 };
 
 class Inventory {
-    private:
-        std::vector<Item> items;
-    public:
-        void addItem(const Item& item);
-        void dropItem(const std::string& itemName);
-        void viewInventory() const;
+private:
+    std::vector<Item> items;
+public:
+    void addItem(const Item& item);
+    void dropItem(const std::string& itemName);
+    void viewInventory() const;
+    bool hasItem(const std::string& itemName) const;
 };
 
-#endif 
+#endif // INVENTORY_H
--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -2,25 +2,46 @@
 
 using namespace std;
 
-Item::Item(string name) : name(name) {}
+Item::Item(string name, string desc, int qty) : name(name), description(desc), quantity(qty) {}
 
 void Inventory::addItem(const Item& item) {
+    // check if item already exists in inventory
+    for (auto& existingItem : items) {
+        if (existingItem.name == item.name) {
+            existingItem.quantity += item.quantity;
+            return;
+        }
+    }
+    // if doesnt exist then add item
     items.push_back(item);
 }
 
 void Inventory::dropItem(const string& itemName) {
     for (auto it = items.begin(); it != items.end(); ) {
         if (it->name == itemName) {
-            it = items.erase(it);
-        } else {
-            ++it;
+            if (it->quantity > 1) {
+                --(it->quantity);
+            } else {
+                it = items.erase(it);
+                continue;
+            }
         }
+        ++it;
     }
 }
 
 void Inventory::viewInventory() const {
     cout << "Inventory:" << endl;
     for (const auto& item : items) {
-        cout << item.name << endl;
+        cout << item.name << " - Quantity: " << item.quantity << endl;
     }
+}
+
+bool Inventory::hasItem(const std::string& itemName) const {
+    for (const auto& item : items) {
+        if (item.name == itemName && item.quantity > 0) {
+            return true;
+        }
+    }
+    return false;
 }
