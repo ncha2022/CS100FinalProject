38c567f4c81d294a934166f5c044ce076a55a904
jwang705 <jwang705@ucr.edu>
2023-12-01 16:19:11 -0800

    small gamble adjustments

--- gamble.cpp
+++ gamble.cpp
@@ -14,29 +14,34 @@ void gamble() { //need adjustments for accounting for opponent's stats
 
     double chance;
     int damage = action.getDamage();
-    char input;
+    char input = 'x';
 
     cout << "Would you like to stake some money?\n";
     while (input != 'y' || input != 'n') {
         cout << "y or n?\n";
         cin >> input;
         if (input != 'y' || input != 'n') {
-            cout << "Invalid input. Please enter 'y' for yes or 'n' for no\n";
+            cout << "Invalid input. Please enter 'y' for yes or 'n' for no.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     }
 
+    
+
     if (input == 'y') {
         // gamble mechanic will generate a payout of # multiplier depending on numbers bet on
         cout << "What numbers are you betting on from 1 to 20?\n";
+        cout << "Please enter your numbers with spaces in between.\n";
+        cout << "Example: 1 5 10 17 20\n";
+
         //create array of pointers? need to figure out how to properly store and read input
     }
     else {
         //change chance formula according to action stats (whether it depends on strength, wisdom, intelligence, etc)
         cout << "You have decided not to stake any money.\n";
         chance = (strength + dex + consitution + intelligence + wisdom + charisma) / 6;
-        cout << "You have a " << chance << "% chance of effectiveness\n";
+        cout << "You have a " << chance << "% chance of effectiveness.\n";
         damage = damage * chance;
         //either print out opponent health or display accordingly based on damage adjustment
     }
